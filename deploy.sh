12793f05e4dcc6aec7f536e04d6f8a6f94b15d46
Maxwell Odri <maxwellodri@gmail.com>
2021-01-25 18:17:46 +1000

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #echo $0
 cd site && yarn build && cd ../
-rsync -urvP "$(dirname $0)/site/build/" root@odri.net.au:/var/www/odri/
+rsync -urvP --delete-after "$(dirname $0)/site/build/" root@odri.net.au:/var/www/odri/
